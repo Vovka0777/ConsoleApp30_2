822a3041d9ec296fa7515564fe337fc7907dd3e0
artem <lidiazvagina5@gmail.com>
2025-03-21 12:10:45 +0200

    Добавьте файлы проекта.

---
+++ Magazine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3x
+{
+    internal class Magazine : PrintedProducts
+    {
+        private int tiraj;
+        private int price;
+
+        public int Tiraj { get => tiraj; set => tiraj = value; }
+        public int Price { get => price; set => price = value; }
+
+        public Magazine() { }
+        public Magazine(string name, int tiraj, int price) : base(name)
+        {
+            this.Tiraj = tiraj;
+            this.Price = price;
+            this.Name = name;
+        }
+        public override void TirajPrice()
+        {
+            int tirajprice = Price * Tiraj;
+            Console.WriteLine($"Стоимость журнала: {tirajprice} рублей");
+        }
+        public override void Info()
+        {
+            Console.WriteLine($"Название журнала: {Name}\nЦена: {Price}\nТираж: {Tiraj}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3x
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //PrintedProducts newpaper = new NewPaper();
+            //PrintedProducts magazine = new Magazine();
+
+            //object[] newpapermass = new object[3];
+            //Console.WriteLine("Введите имя газеты - ");
+            //newpapermass[0] = Convert.ToString(Console.ReadLine());
+            //Console.WriteLine("Введите кол-во листов - ");
+            //newpapermass[1] = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите тираж - ");
+            //newpapermass[2] = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите цену листа - ");
+            //newpapermass[3] = Convert.ToInt32(Console.ReadLine());
+
+            //object[] magazinemass = new object[2];
+            //Console.WriteLine("Введите имя журнала - ");
+            //magazinemass[0] = Console.ReadLine();
+            //Console.WriteLine("Введите тираж ");
+            //magazinemass[1] = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите цену 1 журнала - ");
+            //magazinemass[0] = Console.ReadLine();
+
+            //for (int i = 0; i < newpapermass.Length; i++)
+            //{
+            //    Console.WriteLine(newpapermass[i]);
+            //}
+            //newpaper.TirajPrice();
+
+            //for (int i = 0; i < magazinemass.Length; i++)
+            //{
+            //    Console.WriteLine(magazinemass[i]);
+            //}
+            //magazine.TirajPrice();
+
+            List<PrintedProducts> printedProducts = new List<PrintedProducts>();
+            printedProducts.Add(new Magazine("Весна",30,200));
+            printedProducts.Add(new NewPaper("Правда", 15, 25, 150));
+
+            foreach (PrintedProducts printed in printedProducts)
+            {
+                printed.Info();
+                Console.WriteLine();
+            }
+
+            foreach (PrintedProducts printed in printedProducts)
+            {
+                printed.TirajPrice();
+            }
+            Console.Read();
+        }
+    }
+}
